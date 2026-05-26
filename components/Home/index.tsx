8e63de8c59db11028c759e0055f9db6ba3a744d1
Raunak Mandal <45995056+RaunakMandal@users.noreply.github.com>
2026-05-26 21:37:25 +0530

    Change Designation and Experience

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,30 +1,34 @@
-import Image from 'next/image';
-import profilePic from '../../public/images/raunak.jpg';
-import Link from 'next/link';
-import { ICONS } from '@/core/icons';
+import Image from "next/image";
+import profilePic from "../../public/images/raunak.jpg";
+import Link from "next/link";
+import { ICONS } from "@/core/icons";
 
 export const Home = () => {
   return (
-    <div className='flex flex-col items-center gap-4'>
-      <div className='avatar'>
-        <div className='w-64 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2'>
-          <Image src={profilePic} loading='lazy' alt='Picture of the author' />
+    <div className="flex flex-col items-center gap-4">
+      <div className="avatar">
+        <div className="w-64 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+          <Image src={profilePic} loading="lazy" alt="Picture of the author" />
         </div>
       </div>
-      <p className='text-4xl font-bold text-center'>Hi, I am Raunak Mandal</p>
-      <p className='text-xl text-center'>Senior Software Engineer</p>
-      <div className='flex gap-4 mt-4'>
-        <Link href='mailto:raunak@raunakmandal.is-a.dev'>
-          <button className='btn btn-soft btn-primary'>
-            <span className='flex items-center gap-2'>
+      <p className="text-4xl font-bold text-center">Hi, I am Raunak Mandal</p>
+      <p className="text-xl text-center">Software Engineer</p>
+      <div className="flex gap-4 mt-4">
+        <Link href="mailto:raunak@raunakmandal.is-a.dev">
+          <button className="btn btn-soft btn-primary">
+            <span className="flex items-center gap-2">
               {ICONS.wave}
               Hire me
             </span>
           </button>
         </Link>
-        <Link href='/resume/Raunak_Resume_New.pdf' download={true}>
-          <button className='btn btn-outline'>
-            <span className='flex items-center gap-2'>
+        <Link
+          href="/resume/Raunak_Resume_New.pdf"
+          download={true}
+          target="_blank"
+        >
+          <button className="btn btn-outline">
+            <span className="flex items-center gap-2">
               {ICONS.download}
               Download Résumé
             </span>
